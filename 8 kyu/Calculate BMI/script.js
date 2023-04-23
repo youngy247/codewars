53c1cf4ea62ea57a855952b6e51093ab10a0def7
Adam Young <adamyoungy678@gmail.com>
2023-04-23 13:24:12 +0100

    complete solution

---
+++ 8 kyu/Calculate BMI/script.js
@@ -0,0 +1,13 @@
+function bmi(weight, height) {
+    let bmi = weight/(height**2)
+    if (bmi <= 18.5){
+        return "Underweight"
+    } else if (bmi <= 25.0){
+        return "Normal"
+    } else if (bmi <= 30.0){
+        return "Overweight"
+    } else if (bmi > 30){
+        return "Obese"
+    }
+    return "Invalid input"
+}
